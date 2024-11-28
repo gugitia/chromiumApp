e75f672b93724ea2279241acdc99045a266e2336
gugitia <61124594+gugitia@users.noreply.github.com>
2024-11-28 17:00:55 -0300

    Update index.js

--- src/components/tapbar/index.js
+++ src/components/tapbar/index.js
@@ -22,7 +22,7 @@ export default function Tapbar() {
     <div className="tapbar-container">
       <Link to="/">
         <img
-          src="/images/drake4 (1).png"
+          src="/images/drake.png"
           alt="logo-chromium"
           className="logo-image"
         />
